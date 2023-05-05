13848c04162402f2c02dc8fcd55deed3c944bd94
Janis <janis.paberzs18@gmail.com>
2023-05-05 21:45:40 +0300

    add 02 page

--- public/libs/swiper/core/core.js
+++ public/libs/swiper/core/core.js
@@ -350,8 +350,7 @@ class Swiper {
       }
     } else {
       if ((swiper.params.slidesPerView === 'auto' || swiper.params.slidesPerView > 1) && swiper.isEnd && !swiper.params.centeredSlides) {
-        const slides = swiper.virtual && swiper.params.virtual.enabled ? swiper.virtual.slides : swiper.slides;
-        translated = swiper.slideTo(slides.length - 1, 0, false, true);
+        translated = swiper.slideTo(swiper.slides.length - 1, 0, false, true);
       } else {
         translated = swiper.slideTo(swiper.activeIndex, 0, false, true);
       }
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 # 0039DigitalBreakthrough
 
 <https://megafronteam.github.io/0039DigitalBreakthrough/public>
+<https://megafronteam.github.io/0039DigitalBreakthrough/public/02-news.html>
 <https://megafronteam.github.io/0039DigitalBreakthrough/public/hackathons.html?eventId=969065>
