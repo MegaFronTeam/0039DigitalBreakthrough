4798586a925d33321f44c73962de183565328480
Pavel <pakhal1414@gmail.com>
2023-08-09 14:01:10 +0200

    done ьфз

--- public/js/article.js
+++ public/js/article.js
@@ -1,3 +1,4 @@
+"use strict"
 var params = window
 	.location
 	.search
--- sourse/pug/blocks/sMap/script.js
+++ sourse/pug/blocks/sMap/script.js
@@ -1,28 +1,119 @@
 
-function setMapStats(items) { 
-	
-	let allHackathons = 0;
-	let allPlayers = 0;
-	let allTeams = 0;
-	let allResults = 0;
-	for (let key in items) {
-		let subObj = items[key]; 
-		let path =  document.querySelector(`.map-svg g.path-${key}`);
+let arrAll = [];
+function setMapStats(items) {
+	for (let key in items.regions) {
 
-		path.setAttribute("data-hackathons", subObj.events);
-		path.setAttribute("data-players", subObj.attendees);
-		path.setAttribute("data-teams", subObj.teams);
-		path.setAttribute("data-results", subObj.solutions);
+		let subObj = items.regions[key];
+		if (subObj.region === 'Общая статистика') {
+			arrAll = [
+				subObj.events,
+				subObj.attendees['newbie'],
+				subObj.attendees['profi'],
+				subObj.teams['newbie'],
+				subObj.teams['profi'],
+				subObj.solutions['newbie'],
+				subObj.solutions['profi']
+			];
+			setMap()
+		}
+		else{
 
-		allHackathons += +subObj.events;
-		allPlayers += +subObj.attendees;
-		allTeams += +subObj.teams;
-		allResults += +subObj.solutions;
+			let path = document.querySelector(`.map-svg [data-region="${subObj.region}"]`); 
+			path.setAttribute("data-result-hackathons", subObj.events);
+			path.setAttribute("data-result-attendees-newbie", subObj.attendees['newbie']);
+			path.setAttribute("data-result-attendees-profi", subObj.attendees['profi']);
+			path.setAttribute("data-result-teams-newbie", subObj.teams['newbie']);
+			path.setAttribute("data-result-teams-profi", subObj.teams['profi']);
+			path.setAttribute("data-result-solutions-newbie", subObj.solutions['newbie']);
+			path.setAttribute("data-result-solutions-profi", subObj.solutions['profi']);
+		}
 	}
-	document.querySelector(".sMap__item .hackathons").innerHTML = allHackathons;
-	document.querySelector(".sMap__item .players").innerHTML = allPlayers;
-	document.querySelector(".sMap__item .teams").innerHTML = allTeams;
-	document.querySelector(".sMap__item .results").innerHTML = allResults;
+
+
+	let map = document.querySelector('.map-svg'); 
+		let mapParts = map.children;
+		let numbersElements = document.querySelectorAll(".sMap__number") 
+
+		document.addEventListener('click', function (e) {
+			let target = e.target.closest('[data-region]');
+			let select = e.target.closest('.sMap__select-wrap select'); 
+
+			if (target) {
+				for (const mapPart of mapParts) {
+					mapPart.classList.remove('active');
+				}
+				target.classList.add('active');
+				
+				let index = 0;
+				let attributeNodeArray = [...target.attributes].filter(item => item.name.includes('data-result')); 
+				for (const el of numbersElements) {  
+					counter(el,  +attributeNodeArray[index]['value'], 200);
+					index++;
+				} 
+			}
+			else if (!$(mapParts).hasClass('active') && !select) {
+				for (const mapPart of mapParts) {
+					mapPart.classList.remove('active');
+				}
+				setMap()
+				console.log('00');
+			}
+			
+		});
+
+	let select = document.querySelector('.sMap__select-wrap select');
+	select.addEventListener('change', function (e) {
+		let value = this.value;
+		let paths = document.querySelectorAll(`.map-svg [data-region]`);
+		for (const path of paths) {
+			if (path.getAttribute('data-region') !== value) {
+				path.classList.remove('active');
+				console.log(0);
+      }
+      else {
+				path.classList.add('active');
+				let index = 0;
+				let attributeNodeArray = [...path.attributes].filter(item => item.name.includes('data-result'));
+				for (const el of numbersElements) {
+					counter(el, +attributeNodeArray[index]['value'], 200);
+					index++;
+				}
+				console.log(1);
+      }
+		}
+	});
 
 }
-getMapStats(setMapStats);
+
+function counter(id, end) {
+	
+	let obj = id,
+		current = +id.textContent,
+		start = current,
+		range = (start > 200 || end > 200) ? (end - start) * 2 : end - start,
+		increment = end > start ? 1 : -1,
+		step = Math.abs(Math.floor(200 / range));
+		increment = (start > 300 || end > 300) ? increment * 5 : increment;
+	if (end == start) return;
+	let timer = setInterval(() => {
+		current += increment;
+		obj.textContent = current;
+		if (increment > 0 && current >= end) {
+			current == end;
+			clearInterval(timer);
+		}
+		else if (increment < 0 && current <= end) {
+      current == end;
+      clearInterval(timer);
+    }
+	}, step); 
+}
+
+function setMap(arr = arrAll) {
+	let numbersElements = document.querySelectorAll(".sMap__number")
+	let index = 0;
+	for (const el of numbersElements) {
+		counter(el, + arr[index++]);
+	}
+}
+getMainPageData(setMapStats);
